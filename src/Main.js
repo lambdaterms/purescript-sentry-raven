bb13eb0ba45f6047b3aba10831811fe8735e96d3
Michał Adamczyk <michal.anks@gmail.com>
2018-04-14 20:35:02 +0200

    Add wrappers for Context manipulation functions.

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,20 @@
 // module Sentry
 
 var Raven = require('raven');
+// foreign import setContextImpl ∷ ∀ eff. EffFn2 (raven ∷ RAVEN | eff) Raven Foreign Unit
+
+
+exports.setContextImpl = function(raven, ctx){
+    raven.setContext(ctx);
+};
+
+exports.mergeContextImpl = function(raven, ctx){
+    raven.mergeContext(ctx);
+};
+
+exports.getContextImpl = function(raven){
+    return raven.getContext();
+};
 
 exports.ravenImpl = function(dsn) {
   return new Raven.Client(dsn);
@@ -13,10 +27,17 @@ exports.captureMessageImpl = function(raven, msg) {
   raven.captureMessage(msg);
 };
 
-exports.inContextImpl = function(raven, eff) {
-  return raven.context(eff);
+// -> this is evil:
+// exports.extendContextImpl = function(ctx){
+//     return function(newCtx){
+//         return function(){
+//             return extend(ctx, newCtx);
+//         };};};
+
+exports.inContextImpl = function(raven, ctx, eff) {
+    return raven.context(ctx, eff);
 };
 
 exports.throw = function() {
-  return 8;
+  throw new Error("Test error");
 };
--- src/Main.purs
+++ src/Main.purs
@@ -3,13 +3,19 @@ module Main where
 import Prelude
 
 import Control.Monad.Eff (Eff, kind Effect)
-import Control.Monad.Eff.Console (CONSOLE, log, logShow)
-import Control.Monad.Eff.Uncurried (EffFn2, runEffFn2, EffFn1, runEffFn1)
+import Control.Monad.Eff.Console (CONSOLE, logShow, log)
+import Control.Monad.Eff.Uncurried (EffFn3, runEffFn3, EffFn2, runEffFn2, EffFn1, runEffFn1)
+import Data.Foreign (Foreign, toForeign)
 import Node.Process (PROCESS, lookupEnv)
+import Simple.JSON (write, class WriteForeign)
 import Data.Maybe (maybe)
-import Debug.Trace (traceAnyA)
-import Unsafe.Coerce (unsafeCoerce)
 
+-- debug
+import Debug.Trace (traceAnyA)
+-- import Unsafe.Coerce (unsafeCoerce)
+
+
+foreign import data Context ∷ Type
 foreign import data Raven ∷ Type
 foreign import data RAVEN ∷ Effect
 
@@ -19,21 +25,82 @@ foreign import ravenImpl ∷ ∀ eff. EffFn1 (raven ∷ RAVEN | eff) String Rave
 
 foreign import captureMessageImpl ∷ ∀ eff. EffFn2 (raven ∷ RAVEN | eff) Raven String Unit
 
-foreign import inContextImpl ∷ ∀ a eff. EffFn2 (raven ∷ RAVEN | eff) Raven (Eff (raven ∷ RAVEN | eff) a) a
+foreign import inContextImpl ∷ ∀ a eff. EffFn3 (raven ∷ RAVEN | eff) Raven Foreign (Eff (raven ∷ RAVEN | eff) a) a
+
+foreign import mergeContextImpl ∷ ∀ eff. EffFn2 (raven ∷ RAVEN | eff) Raven Foreign Unit
+
+foreign import setContextImpl ∷ ∀ eff. EffFn2 (raven ∷ RAVEN | eff) Raven Foreign Unit
+
+foreign import getContextImpl ∷ ∀ eff. EffFn1 (raven ∷ RAVEN | eff) Raven Context
+
+-- czy to jest bezpieczne
+-- foreign import extendContextImpl ∷ Context → Foreign → Context
+
+-- debug
+foreign import throw ∷ ∀ eff. Eff eff Int
+
+instance writeForeighnContextInstance ∷ WriteForeign Context where
+  writeImpl = toForeign
 
 raven ∷ ∀ eff. Dsn → Eff (raven ∷ RAVEN | eff) Raven
 raven (Dsn s) = runEffFn1 ravenImpl s
 
 captureMessage ∷ ∀ eff. Raven → String → Eff (raven ∷ RAVEN | eff) Unit
-captureMessage r msg = runEffFn2 captureMessageImpl r msg
+captureMessage = runEffFn2 captureMessageImpl
 
-inContext ∷ ∀ a eff. Raven → Eff (raven ∷ RAVEN | eff) a → Eff (raven ∷ RAVEN | eff) a
-inContext r eff = runEffFn2 inContextImpl r eff
+-- context
+inContext ∷ ∀ a ctx eff.  WriteForeign ctx ⇒ Raven → ctx → Eff (raven ∷ RAVEN | eff) a → Eff (raven ∷ RAVEN | eff) a
+inContext rvn ctx = runEffFn3 inContextImpl rvn (write ctx)
+
+setContext ∷ ∀ ctx eff. WriteForeign ctx ⇒ Raven → ctx → Eff (raven ∷ RAVEN | eff) Unit
+setContext rvn ctx = runEffFn2 setContextImpl rvn (write ctx)
+
+mergeContext ∷ ∀ ctx eff. WriteForeign ctx ⇒ Raven → ctx → Eff (raven ∷ RAVEN | eff) Unit
+mergeContext rvn ctx = runEffFn2 mergeContextImpl rvn (write ctx)
+
+-- extendContext ∷ ∀ ctx. WriteForeign ctx ⇒ Context → ctx → Context
+-- extendContext old new = extendContextImpl old (write new)
+
+getContext ∷ ∀ eff. Raven → Eff (raven ∷ RAVEN | eff) Context
+getContext = runEffFn1 getContextImpl
+
+
+---------- main for tests
 main ∷ forall e. Eff (console ∷ CONSOLE, process ∷ PROCESS, raven ∷ RAVEN | e) Unit
 main = do
   dsn ← (Dsn <<< maybe "" id) <$> lookupEnv "SENTRY_DSN"
+  r ← raven dsn
+  setContext r {user : {id : 1}}
+  captureMessage r ("TEST MESSAGE")
+  setContext r {tags : {testTagKey : "testTagValue"}}
+  ctx ← getContext r
+  traceAnyA ctx
 
-foreign import throw ∷ ∀ eff. Eff eff Int
+  captureMessage r ("TEST MESSAGE 2")
+  mergeContext r {user : {id : "testUserId"}}
+
+
+  captureMessage r ("TEST MESSAGE 3")
+
+  mergeContext r {}
+  captureMessage r ("TEST MESSAGE 4")
+
+  setContext r {}
+  captureMessage r ("TEST MESSAGE T")
+
+  -- _ ← throw   ------------------- odkomentowanie tej linijki powoduje, że żaden message ani error nie jest logowany
+
+  x ← inContext r ctx (do
+                        ctx ← getContext r
+                        mergeContext r {user : {email : "test@test.com"}}
+                        log "ctx2:"
+                        traceAnyA ctx -- shows: {}
+                        throw)
+
+  logShow x
+  -- traceAnyA x
+  -- captureMessage r "NO GLOB"
+  -- traceAnyA r
 
 
 
@@ -46,12 +113,3 @@ foreign import throw ∷ ∀ eff. Eff eff Int
 --
 --
 -- u = f _{ email = Just "new", age = Just 8 }
-
-
-  captureMessage r ("TEST MESSAGE")
-  -- x ← y r
-  x ← inContext r throw
-  logShow x
-  -- traceAnyA x
-  -- captureMessage r "NO GLOB"
-  -- traceAnyA r
